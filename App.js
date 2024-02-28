dd7e1645c62c712171666ed1192cf9d6103e47b3
Boglarka <boglarkaferencsik05@gmail.com>
2024-02-28 08:38:06 +0100

    Alap kész

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.cim}>Testtömegindex</Text>
+
+      <View style={styles.body}>
+        <Text style={styles.szoveg}>Tömeg</Text>
+        <TextInput
+          style={styles.mezo}
+          onChangeText={text => setWeight(text)}
+          value = {weight}
+        />
+        <Text style={styles.szoveg}>Magasság</Text>
+        <TextInput
+          style={styles.mezo}
+          onChangeText={text => setHeight(text)}
+          value = {height}
+        />
+        <Pressable
+          onPress={calcBMI}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>
+            SZÁMÍT
+          </Text>
+        </Pressable>
+        <Text style={styles.szoveg}>Testtömegindex</Text>
+        <TextInput
+          style={styles.mezo}
+          value = {bmi}
+        />
+
+        <Pressable
+            onPress={reset}
+            style={styles.button}>
+            <Text style={styles.buttonText}>VISSZAÁLLÍT</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Ferencsik Boglárka, 2024</Text>
+      </View>
+
     </View>
   );
 }
@@ -13,7 +52,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightpink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  body: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
